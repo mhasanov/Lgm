6ff0559af3005d114989b0568d9764953ec9b26a
mhasanov <mhasanov@vt.edu>
2022-01-03 13:19:52 -0500

    a

--- LIvelihoodGeneratingMachineArchitecture/Shared/App.js
+++ LIvelihoodGeneratingMachineArchitecture/Shared/App.js
@@ -21,6 +21,10 @@ export default function App() {
 
   
   return (
-    RootStackScreen()
+    userToken == null ? (
+      RootStackScreen()
+    ) : (
+      <Text>Signed In</Text>
+    )
   );
 }
