d866c89e3ababb1212c8964cc113631022b612b7
Meng Fan Wang <mengfanwang724@gmail.com>
2022-02-05 18:13:11 -0500

    updated question pg

--- LIvelihoodGeneratingMachineArchitecture/Shared/app/components/QuestionBox.js
+++ LIvelihoodGeneratingMachineArchitecture/Shared/app/components/QuestionBox.js
@@ -1,19 +1,32 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import { globalStyles } from '../assets/styles/global';
 
 
 const QuestionBox = ({name, difficulty}) => {
     return(
-        <TouchableOpacity style={[globalStyles.buttonContainer]}>
-            <View>
-                <Text style={[globalStyles.buttonText]}>{name}</Text>
+        <TouchableOpacity>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>{name} ({difficulty})</Text>
             </View>
-            <View>
-                <Text style={[globalStyles.buttonText]}>{difficulty}</Text>
-            </View>
+            
         </TouchableOpacity>
     );
 };
+
+const styles = StyleSheet.create({
+    button:{
+        backgroundColor: "#C11F83",
+        paddingVertical: 12,
+        paddingHorizontal: 25,
+        borderRadius: 25,
+        marginTop: 10
+    },
+    buttonText:{
+        color: "white",
+        fontSize: 15,
+        textAlign: "center"
+    }
+});
 
 export default QuestionBox;
--- LIvelihoodGeneratingMachineArchitecture/Shared/app/screens/Questions.js
+++ LIvelihoodGeneratingMachineArchitecture/Shared/app/screens/Questions.js
@@ -1,8 +1,25 @@
 import React, {useState} from "react";
-import { Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions, Platform, PixelRatio } from 'react-native';
 import { globalStyles, images } from '../assets/styles/global';
 import { QuestionsInfo } from "../utils/QuestionsInfo";
 import QuestionBox from "../components/QuestionBox";
+
+const {
+    width: SCREEN_WIDTH,
+    heigh: SCREEN_HEIGHT,
+}= Dimensions.get('window')
+
+const scale = SCREEN_WIDTH/375
+
+function normalize(size){
+    const newSize = size * scale
+    if (Platform.OS == 'ios'){
+        return Math.round(PixelRatio.roundToNearestPixel(newSize))
+    }
+    else{
+        return Math.round(PixelRatio.roundToNearestPixel(newSize)) -2
+    }
+}
 
 const Questions = ({ navigation }) => {
 
@@ -35,45 +52,48 @@ const Questions = ({ navigation }) => {
             );
         }
     }
-    /*
-
-    var not_started = [];
-
-
-    //TODO:
-    for (let i = 0; i < 1; i++) {               //Replace 1 with 75
-        //if question is not started    //Check user database
-        not_started.push(
-            <View key={i}>
-                <Text>{Questions_json.name[0]}</Text>
-            </View>
-        );
-    }
-      
-    */
-      
-    return(
-        <View style={globalStyles.loginContainer}>
-            <View>
-            </View>
-            <View>
-                <View>
-                    <Text>Not Started</Text>
-                    {questionNumbersNotComplete}
-                </View>
-
-                <View>
-                    <Text>In Progress</Text>
-                    {questionNumbersInProgress}
-                </View>
-
-                <View>
-                    <Text>Complete</Text>
-                    {questionNumbersComplete}
-                </View>
-            </View>
+    
+    return (
+        <View style={[styles.container, {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: "row"
+        }]}>
+          <View style={{ flex: 1, backgroundColor: "#DFC9F8" }}>
+                <Text numberOfLines={1}
+                    adjustsFontSizeToFit
+                    style = {styles.titleText}>
+                    Not Started
+                </Text>
+                {questionNumbersNotComplete}
+          </View>
+          <View style={{ flex: 1, backgroundColor: "#944AE8" }}>
+                <Text style = {styles.titleText}>In Progress</Text>
+                {questionNumbersInProgress}
+          </View>
+          <View style={{ flex: 1, backgroundColor: "#6117B5" }}>
+                <Text style = {styles.titleText}>Completed</Text>
+                {questionNumbersComplete}
         </View>
-    );
+        </View>
+      );
 };
+
+const styles = StyleSheet.create({
+    baseText: {
+        fontFamily: "Cochin",
+        fontSize: normalize(10),
+    },
+    titleText: {
+        fontSize: normalize(10),
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    container: {
+      flex: 1,
+    },
+    button:{
+        color:"#3C0F6F"
+    }
+});
 
 export default Questions;
