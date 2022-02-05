7376bb289247adad11d0000e4cd0c4327c2ce6b1
mhasanov <49108810+mhasanov@users.noreply.github.com>
2022-02-05 12:04:57 -0500

    Update Questions.js

--- LIvelihoodGeneratingMachineArchitecture/Shared/app/screens/Questions.js
+++ LIvelihoodGeneratingMachineArchitecture/Shared/app/screens/Questions.js
@@ -1,9 +1,40 @@
 import React, {useState} from "react";
 import { Text, View, Image } from 'react-native';
 import { globalStyles, images } from '../assets/styles/global';
+import { QuestionsInfo } from "../utils/QuestionsInfo";
+import QuestionBox from "../components/QuestionBox";
 
 const Questions = ({ navigation }) => {
 
+    var stringOf75BoolValues = "0121"//Check user database
+
+    var questionNumbersNotComplete = []
+    var questionNumbersInProgress = []
+    var questionNumbersComplete = []
+
+    for (let i = 0; i < stringOf75BoolValues.length; i++) {
+        if(stringOf75BoolValues.charAt(i) == '0'){
+            questionNumbersNotComplete.push(
+                <View key={i}>
+                    <QuestionBox name={QuestionsInfo.name[i]} difficulty={QuestionsInfo.difficulty[i]}></QuestionBox>
+                </View>
+            );
+        }
+        else if(stringOf75BoolValues.charAt(i) == '1'){
+            questionNumbersInProgress.push(
+                <View key={i}>
+                    <QuestionBox name={QuestionsInfo.name[i]} difficulty={QuestionsInfo.difficulty[i]}></QuestionBox>
+                </View>
+            );
+        }
+        else if(stringOf75BoolValues.charAt(i) == '2'){
+            questionNumbersComplete.push(
+                <View key={i}>
+                    <QuestionBox name={QuestionsInfo.name[i]} difficulty={QuestionsInfo.difficulty[i]}></QuestionBox>
+                </View>
+            );
+        }
+    }
     /*
 
     var not_started = [];
@@ -28,14 +59,17 @@ const Questions = ({ navigation }) => {
             <View>
                 <View>
                     <Text>Not Started</Text>
+                    {questionNumbersNotComplete}
                 </View>
 
                 <View>
                     <Text>In Progress</Text>
+                    {questionNumbersInProgress}
                 </View>
 
                 <View>
                     <Text>Complete</Text>
+                    {questionNumbersComplete}
                 </View>
             </View>
         </View>
