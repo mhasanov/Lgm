44914b08f53703e44b45974fb5f6e713040a13fd
mhasanov <49108810+mhasanov@users.noreply.github.com>
2022-02-05 15:52:26 -0500

    Update RootStackScreen.js

--- LIvelihoodGeneratingMachineArchitecture/Shared/app/screens/RootStackScreen.js
+++ LIvelihoodGeneratingMachineArchitecture/Shared/app/screens/RootStackScreen.js
@@ -2,30 +2,15 @@ import React from 'react'
 import { Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import LoginScreen from "./LoginScreen";
-import SignUpScreen from "./SignUpScreen";
 import SplashScreen from './SplashScreen';
 import { View } from 'react-native-animatable';
 import Icon from 'react-native-vector-icons/AntDesign';
 import Questions from './Questions';
+import QuestionScreen from './QuestionScreen';
 
 const Stack = createStackNavigator()
 
-/*
 
-        <Stack.Screen name='Home' component={SplashScreen} options={{
-          headerRight: () => (
-            <Icon.Button name="google" size={30} color="#" />
-          ),
-        }}/>
-
-
-        options={({ navigation, route }) => ({
-          headerTitle: props => <LogoTitle {...props} />,
-        })}
-      />
-
-*/
 export default function RootStackScreen() {
   return (
     <NavigationContainer>
@@ -36,16 +21,26 @@ export default function RootStackScreen() {
             <View>
               <Icon.Button name="google" size={30} color="#" />
               <Button onPress={() => navigation.navigate('Questions')} title="Questions" color="#841584" accessibilityLabel="Questions" />
-            
             </View>
           ),
         })}/>
 
         <Stack.Screen name='Questions' component={Questions} options={{
           headerRight: () => (
-            <Icon.Button name="google" size={30} color="#" />
+            <Icon.Button  name="google" size={30} color="#" />
           ),
         }}/>
+
+
+        <Stack.Screen name='QuestionScreen' component={QuestionScreen} ons={({navigation}) => ({
+          headerRight: () => (
+            <View>
+              <Icon.Button name="google" size={30} color="#" />
+              <Button onPress={() => navigation.navigate('Questions')} title="Questions" color="#841584" accessibilityLabel="Questions" />
+            </View>
+          ),
+        })}/>
+
       </Stack.Navigator>
     </NavigationContainer>
   );
